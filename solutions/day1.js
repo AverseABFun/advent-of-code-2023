4f102f9752f82f80d379085371253b8e9fd57051
AverseABFun <averse.abfun@gmail.com>
2023-12-01 07:39:29 -0600

    working on part 2

--- solutions/day1.js
+++ solutions/day1.js
@@ -20,12 +20,43 @@ fs.readFile('../input/day1', 'utf8', (err, data) => {
         console.error(err);
         return;
     }
-    let adjustedData = data.split("\n");
+    let splitData = data.split("\n");
     let results = [];
-    for (let i = 0; i<adjustedData.length; i++) {
-        let item = String(adjustedData[i]);
+    for (let i = 0; i<splitData.length; i++) {
+        let item = String(splitData[i]);
+        let digits = ["_"];
+        for (let a = 0; a<999; a++) {
+            digits[a] = digits[0];
+        }
+        for (let d = 0; d<Object.keys(letterNumsToRealNums).length; d++) {
+            let digit = Object.keys(letterNumsToRealNums)[d];
+            let startPos = -1;
+            for (let o = 0; o<item.length; o++) {
+                if (item[o] === digit[0]) {
+                    startPos = o;
+                }
+            }
+            if (startPos !== -1) {
+                let numCorrect = 1;
+                for (let o = 1; o<digit.length; o++) {
+                    if (digit[o] === item[startPos+o]) {
+                        numCorrect++;
+                    } else {
+                        numCorrect -= Infinity;
+                    }
+                }
+                console.log(`${digit} is correct at position ${i+1}`);
+                if (numCorrect === digit.length) {
+                    digits[startPos] = letterNumsToRealNums[digit];
+                }
+            }
+        }
         item = item.replaceAll(/[a-z]/ig,"");
-        results.push(parseInt(item[0]+item[item.length-1]));
+        for (var s = 0; s<item.length; s++) {
+            digits[s] = item[s];
+        }
+        digits = digits.join('').replaceAll("_","");
+        results.push(parseInt(digits[0]+digits[digits.length-1]));
     }
     let sum = 0;
     for (let i = 0; i<results.length; i++) {
