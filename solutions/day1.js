a2827a7698fff4c32a69f824b6bdd579e331480a
AverseABFun <averse.abfun@gmail.com>
2023-12-01 07:10:05 -0600

    began part 2 of day 1

--- solutions/day1.js
+++ solutions/day1.js
@@ -2,6 +2,19 @@
 
 const fs = require('fs');
 
+const letterNumsToRealNums = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+};
+
 fs.readFile('../input/day1', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
